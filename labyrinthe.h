98b5230e5f401f422d82eae21e690ac2089c386a
kilianpoulin <kilian.poulin@efrei.net>
2016-04-09 01:31:02 +0200

    Génération du labyrinthe - DONE

--- labyrinthe.h
+++ labyrinthe.h
@@ -35,12 +35,13 @@ SDL_Texture *load_texture(char path[100] , SDL_Renderer *renderer);
 
 int generer_labyrinthe(Labyrinthe *labyrinthe, int colonneActuel, int ligneActuel, int *colonneSuivante, int *ligneSuivante);
 
-int tirer_direction(Labyrinthe *labyrinthe, const int xActuel, const int yActuel, int *xSuiv, int *ySuiv, int nb);
-int tirer_direction_backtracking(Labyrinthe *labyrinthe, int xActuel, int yActuel, int *xSuiv, int *ySuiv, int nb);
-void melange(int *direction , int taille);
 
-enum{VISITE , MUR_NORD , MUR_SUD , MUR_EST , MUR_OUEST};
-enum{IMG_MUR_H , IMG_MUR_V};
-enum{NORD,EST,SUD,OUEST};
+int tirer_direction(Labyrinthe *labyrinthe, const int xActuel, const int yActuel, int *xSuiv, int *ySuiv, int nb); // Fonction qui génère la case suivante
+int tirer_direction_backtracking(Labyrinthe *labyrinthe, int xActuel, int yActuel, int *xSuiv, int *ySuiv, int nb); //Fonction qui réalise le backtracking
+void melange(int *direction , int taille); // melange aléatoirement les directions dans un tableau
+
+enum{VISITE , MUR_NORD , MUR_SUD , MUR_EST , MUR_OUEST}; // Informations sur les cases
+enum{IMG_MUR_H , IMG_MUR_V};    // Images des murs (barre horizontale et barre verticale
+enum{NORD,EST,SUD,OUEST};   // Directions
 
 #endif //SDL_TEST_LABYRINTHE_H
